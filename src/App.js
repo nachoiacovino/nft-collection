d3328f1f70adbceb536a270e8d812c0cd54b8719
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-10-10 03:30:03 +0200

    Add login with wallet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './styles/App.css';
+
+import React, { useEffect, useState } from 'react';
+
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -9,12 +11,82 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    /*
+* Check if we're authorized to access the user's wallet
+*/
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length > 0) {
+      const [account] = accounts;
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  /*
+* Implement your connectWallet method here
+*/
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+
   // Render Methods
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
+  const renderNotConnectedContainer =
+    currentAccount ? (
+      <button onClick={null} className="cta-button connect-wallet-button">
+        Mint NFT
+      </button>
+    ) : (
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect to Wallet
+      </button>
+    );
 
   return (
     <div className="App">
@@ -24,7 +96,7 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {renderNotConnectedContainer}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
