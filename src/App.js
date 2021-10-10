41aafec72aed4bbbeb5f27b1ee62a42f1f358245
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-10-10 09:38:25 +0200

    Add listener to get NFT id

--- src/App.js
+++ src/App.js
@@ -12,35 +12,46 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0x16e7c27AddEC9a40091D59a5e7a48B3e953fF596";
+
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletIsConnected = async () => {
-    /*
-    * First make sure we have access to window.ethereum
-    */
-    const { ethereum } = window;
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    const checkIfWalletIsConnected = async () => {
+      /*
+      * First make sure we have access to window.ethereum
+      */
+      const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
-    }
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
 
-    /*
-* Check if we're authorized to access the user's wallet
-*/
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+      /*
+  * Check if we're authorized to access the user's wallet
+  */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-    if (accounts.length > 0) {
-      const [account] = accounts;
-      console.log("Found an authorized account:", account);
-      setCurrentAccount(account);
-    } else {
-      console.log("No authorized account found");
-    }
-  };
+      if (accounts.length > 0) {
+        const [account] = accounts;
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        // Setup listener! This is for the case where a user comes to our site
+        // and ALREADY had their wallet connected + authorized.
+        setupEventListener();
+      } else {
+        console.log("No authorized account found");
+      }
+    };
+    checkIfWalletIsConnected();
+  }, []);
 
   /*
 * Implement your connectWallet method here
@@ -64,13 +75,46 @@ const App = () => {
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+        });
+
+        console.log("Setup event listener!");
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x1C7Ea5BD7A96408Ab59dA3121df576BCfA2b0Bb7";
     try {
       const { ethereum } = window;
 
@@ -94,15 +138,6 @@ const App = () => {
       console.log(error);
     }
   };
-
-
-  /*
-  * This runs our function when the page loads.
-  */
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
-
 
   // Render Methods
   const renderNotConnectedContainer =
